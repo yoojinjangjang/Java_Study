4f12a94bc3fbc07daf0a8a49a5257ef1a613df3b
yoojin Jang <yoojinjangjang@gmail.com>
2022-02-01 00:01:46 +0900

    2/1

---
+++ ch04/src/ch04/FlowEx5.java
@@ -0,0 +1,41 @@
+package ch04;
+
+import java.util.Scanner;
+
+public class FlowEx5 {
+
+	public static void main(String[] args) {
+		int score = 0;
+		char grade = ' ',opt = '0';
+		
+		System.out.print("점수를 입력해주세요.>");
+		
+		Scanner scanner = new Scanner(System.in);
+		score = scanner.nextInt();
+		
+		
+		System.out.printf("당신의 정수는 %d 입니다.%n",score);
+		
+		if ( score >=90) {
+			grade = 'A';
+			if (score >= 98) {
+				opt = '+';
+			}else if(score < 94) {
+				opt = '-';
+			}
+		}else if (score >=80) {
+			grade = 'B';
+			if (score >= 88) {
+				opt = '+';
+			}else if(score < 84) {
+				opt = '-';
+			}
+		}else {
+			grade = 'C';
+		}
+		
+		System.out.printf("당신의 학점은 %c%c 입니다.%n", grade,opt);
+	}
+	
+
+}
